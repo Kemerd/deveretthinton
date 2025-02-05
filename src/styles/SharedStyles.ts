1ca5130100ad15dd9e13fe906ec2145108ad000f
D Everett Hinton <Dragonfabledonny@hotmail.com>
2025-02-05 12:20:27 -0800

    Fixed mobile support for quip

--- src/styles/SharedStyles.ts
+++ src/styles/SharedStyles.ts
@@ -3,4 +3,12 @@ import { AppTheme } from '../theme/theme';
 
 export const QuipText = styled.p`
     ${AppTheme.styledComponents.QuipText}
+    
+    @media (max-width: 1268px) {
+        /* Allow text to wrap naturally on smaller screens */
+        white-space: normal;
+        text-align: center;
+        padding: 0 ${AppTheme.spacing[24]};
+        margin-bottom: ${AppTheme.spacing[16]};
+    }
 `; 
